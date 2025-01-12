1157bc156dc1912c2bc90bed4dece8f26cad720b
Connor Slade <connor@connorcode.com>
2025-01-12 13:07:32 -0500

    Determine path thickness

--- src/main.rs
+++ src/main.rs
@@ -17,15 +17,15 @@ mod geometry;
 mod point;
 use geometry::{close_path, generate_circle, generate_rectangle};
 
-const CIRCLE_SIDES: u32 = 20;
+const CIRCLE_SIDES: u32 = 100;
 
 fn main() -> Result<()> {
     let input = File::open("/home/connorslade/Documents/LibrePCB/projects/Relay Logic/RS_Latch/output/v1/gerber/Relay_Logic_COPPER-TOP.gbr")?;
     let doc = gerber_parser::parser::parse_gerber(BufReader::new(input));
 
-    let path_thickness = 0.5 / 2.0;
+    let mut aperture: Option<&Aperture> = None;
+    let mut thickness = 0.0;
 
-    let mut aperture = None;
     let mut path = Vec::new();
     let mut paths = Vec::new();
 
@@ -33,17 +33,22 @@ fn main() -> Result<()> {
         match cmd {
             Command::FunctionCode(FunctionCode::DCode(code)) => match code {
                 DCode::Operation(Operation::Move(mov)) => {
+                    thickness = match aperture.unwrap() {
+                        Aperture::Circle(circle) => circle.diameter,
+                        _ => 0.0,
+                    };
+
                     if !path.is_empty() {
-                        paths.push(close_path(mem::take(&mut path), path_thickness));
+                        paths.push(close_path(mem::take(&mut path), thickness));
                     }
 
                     let point = mov.into();
-                    paths.push(generate_circle(point, path_thickness / 2.0, CIRCLE_SIDES));
+                    paths.push(generate_circle(point, thickness / 2.0, CIRCLE_SIDES));
                     path.push(point);
                 }
                 DCode::Operation(Operation::Interpolate(pos, _offset)) => {
                     let point = pos.into();
-                    paths.push(generate_circle(point, path_thickness / 2.0, CIRCLE_SIDES));
+                    paths.push(generate_circle(point, thickness / 2.0, CIRCLE_SIDES));
                     path.push(point);
                 }
 
@@ -66,7 +71,7 @@ fn main() -> Result<()> {
     }
 
     if !path.is_empty() {
-        paths.push(close_path(path, path_thickness));
+        paths.push(close_path(path, thickness));
     }
 
     let mut union = vec![vec![paths.remove(0)]];
